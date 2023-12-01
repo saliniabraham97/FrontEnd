f01ce764f9af84e6a98e5be875eca6aa0f9e2c86
Salini M <110709470+saliniabraham97@users.noreply.github.com>
2023-12-01 14:03:45 +0530

    EmailValidation

---
+++ JavaScript/EmailValidation/script.js
@@ -0,0 +1,13 @@
+function validateEmail(){
+    var email = document.getElementById('email').value;
+
+    // Regular expression for email format validation
+    var emailRegex = /^[a-zA-Z0-9._-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+
+    // Check if the email matches the specified criteria
+    if (emailRegex.test(email)) {
+      alert('Valid email address!');
+    } else {
+      alert('Invalid email format. Please enter a valid email address.');
+    }
+}
