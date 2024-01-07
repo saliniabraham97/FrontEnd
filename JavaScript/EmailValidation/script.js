33e0baed2ea24805d40fc794d8c585c7ec4b79aa
Salini M <110709470+saliniabraham97@users.noreply.github.com>
2024-01-07 13:41:21 +0530

    email

--- JavaScript/EmailValidation/script.js
+++ JavaScript/EmailValidation/script.js
@@ -1,6 +1,5 @@
 function validateEmail(){
     var email = document.getElementById('email').value;
-
     // Regular expression for email format validation
     var emailRegex = /^[a-zA-Z0-9._-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
 
