46127eaeb0900a92ec227df5328e8e9762e00340
Salini M <110709470+saliniabraham97@users.noreply.github.com>
2024-02-15 16:16:54 +0530

    default password

---
+++ JavaScript/password length as 8/script.js
@@ -0,0 +1,17 @@
+
+function generatePassword(length = 8) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+  return password;
+}
+
+const defaultPassword = generatePassword();
+
+const userConfirmation = window.prompt("Your generated password is:", defaultPassword);
+
+
